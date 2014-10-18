de54a5f34cdd3f26c5ee45d12c738bb31afa15c9
Michael Hensler <mhensler@microsoft.com>
2014-10-18 14:51:30 -0700

    Implementing makeCacheMatrix/cacheSolve methods for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file implements a pair of methods used for caching the inverse 
+## of a matrix
 
-## Write a short comment describing this function
+## This method takes a matrix and returns a pair of functions
+## for getting the matrix and its inverse. The inverse is lazily cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    get <- function() x
+    getInv <- function(...) {
+        if (is.null(inv)) {
+            inv <<- solve (x, ...)
+        }
+        inv
+    }
+    list (get = get, getInv = getInv)  
 }
 
-
-## Write a short comment describing this function
+## This is a useless function required for the assignment.
+## It simply calls the getInv method on the cacheMatrix passed in.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    x$getInv(...)
 }
